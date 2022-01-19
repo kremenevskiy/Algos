7adbfbab7fb7c73d757ff6e65a46f81d861f61a1
kremenevskiy <kremeneuski@gmail.com>
2022-01-20 00:44:11 +0300

    Applied dynamic programming

--- LeetCode/python/Easy/maximum_subarray.py
+++ LeetCode/python/Easy/maximum_subarray.py
@@ -1,38 +1,34 @@
 from typing import List
 
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         @cache
+#         def solve(i, must_pick):
+#             if i >= len(nums): return 0 if must_pick else -inf
+#             return max(nums[i] + solve(i+1, True), 0 if must_pick else solve(i+1, False))
+#         return solve(0, False)
+
+
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         big, sum = float('-inf'), 0
+#         for i in range(n):
+#             sum += nums[i]
+#             big = max(big, sum)
+#             if sum < 0:
+#                 sum = 0
+#         return big
+
+
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        prevsum = nums[0]
-        sum = nums[0]
-        zerod = False
+    def maxSubArray(self, nums):
+        dp = [*nums]
         for i in range(1, len(nums)):
-            if nums[i] < 0 and abs(nums[i]) > sum:
-                if sum > prevsum:
-                    prevsum = sum
-                if nums[i] > sum:
-                    sum = nums[i]
-                    continue
-                sum = nums[i]
-                zerod = True
-                continue
-            if nums[i] >= 0 and sum < 0:
-                sum = nums[i]
-                continue
-            if nums[i] < 0:
-                if sum > prevsum:
-                    prevsum = sum
-                if nums[i] > sum:
-                    sum = nums[i]
-                    continue
-            sum += nums[i]
-            zerod = False
-        if zerod:
-            if sum > 0:
-                return max(prevsum, sum)
-
-        return max(prevsum, sum)
+            dp[i] = max(nums[i], nums[i] + dp[i-1])
+        return max(dp)
 
 
 
-print(Solution().maxSubArray([-2, -3, -1]))
+print(Solution().maxSubArray([-2, 1]))
 
