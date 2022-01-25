d136f4ec771596835182a23dd2c8ecdcf0ac31f8
kremenevskiy <kremeneuski@gmail.com>
2022-01-25 04:23:51 +0300

    day 6 coding leetcode

--- LeetCode/python/Medium/delete_and_earn.py
+++ LeetCode/python/Medium/delete_and_earn.py
@@ -1,24 +1,15 @@
 from typing import List
 
 
-# class Solution:
-#     def deleteAndEarn(self, nums: List[int]) -> int:
-#         arr = [0] * (max(nums) + 1)
-#         for x in nums:
-#             arr[x] += x
-#         prev, curr = 0, 0
-#         for i in range(min(nums), len(arr)):
-#             prev, curr = curr, max(curr, prev + arr[i])
-#         return curr
-
-
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        points = {}
-        n = 
-        def dp(i):
-            memo[i] = max(dp())
-
+        arr = [0] * (max(nums) + 1)
+        for x in nums:
+            arr[x] += x
+        prev, curr = 0, 0
+        for i in range(min(nums), len(arr)):
+            prev, curr = curr, max(curr, prev + arr[i])
+        return curr
 
 
 print(Solution().deleteAndEarn([3,7,10,5,2,4,8,9,9,4,9,2,6,4,6,5,4,7,6,10]))
